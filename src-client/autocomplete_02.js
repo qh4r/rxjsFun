e1fcf0ba8d32d964ad33d38f10943a29c3e979ed
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-08-22 23:38:35 +0200

    Implemented rx version of autocomplete

---
+++ src-client/autocomplete_02.js
@@ -0,0 +1,30 @@
+import $ from 'jquery'
+import Rx from 'rxjs/Rx'
+
+const $title = $('#title');
+const $results = $('#results');
+
+const keyUps$ = Rx.Observable.fromEvent($title, 'keyup');
+const queries$ = keyUps$
+    .map(e => e.target.value)
+    .distinctUntilChanged()
+    .debounceTime(200)
+    .switchMap(x => {
+        console.log(x);
+        return getItems(x)
+    });
+
+queries$.subscribe(data => {
+        $results.empty();
+        const $items = data.items.map(item => $('<li/>').text(item));
+        $results.append($items);
+});
+
+function getItems(title, querryId) {
+    console.log(`querry to ${title}, id: ${querryId}`);
+    return new Promise((resolve, reject) => {
+        window.setTimeout(() => {
+            resolve({items: [title, 'Item 2', `Another item ${Math.random()}`], id: querryId})
+        }, 500 + (Math.random() * 2000));
+    })
+}
